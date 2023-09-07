132e1e822e2a81a3c3a848d23d5277ccfb751238
KianDoyle <kianrk@icloud.com>
2023-09-08 00:20:34 +0100

    Whole update

--- index.js
+++ index.js
@@ -1,27 +1,64 @@
+const logoText = document.querySelector("#logo-text");
+const background = document.querySelector("#background");
+const logoImg = document.querySelector("#logo-img");
+const polaroid = document.querySelector("#polaroid-wrapper");
+
+const anim1 = gsap.timeline();
+anim1.to(logoText, {
+    opacity: 0,
+    y: "100%", 
+    display: "none", 
+    duration: 0.5, 
+    ease: "ease.out", 
+    delay: 1, 
+});
+
+const anim2 = gsap.timeline();
+anim2.to(background, {
+    width: "825px", 
+    duration: 1, 
+    ease: "power2.inOut", 
+    delay: 1.5, 
+    onComplete: function () {
+        background.style.overflow = "visible";
+    }
+});
+
+const anim3 = gsap.timeline();
+anim3.to(logoImg, {
+    width: "825px", 
+    filter: "blur(15px)", 
+    duration: 1, 
+    ease: "power2.inOut", 
+    delay: 1.5, 
+});
+
+const anim4 = gsap.timeline();
+anim4.to(polaroid, {
+    top: "90px",
+    duration: 0.9, 
+    ease: "ease.inOut",
+    delay: 2.2,
+}).to(polaroid, {
+    top: "100px",
+    duration: 0.2,
+    ease: "bounce.out",
+});
+
 document.addEventListener('DOMContentLoaded', function () {
     const polaroidWrapper = document.getElementById('polaroid-wrapper');
     const removeButton = document.getElementById('remove-welcome-blurb');
     const imageWrapper = document.getElementById('portfolio-images');
 
+    anim1.play();
+    anim2.play();
+    anim3.play();    
+    anim4.play();
+
     removeButton.addEventListener('click', () => {
-        polaroidWrapper.style.cssText += "transform: translateX(-2000px);";
-        imageWrapper.style.cssText += "opacity: 100%;"
+        requestAnimationFrame(() => {
+            polaroidWrapper.style.transform = "translateX(-2000px)";
+            imageWrapper.style.opacity = "100%";
+        });
     });
 });
-
-// const nav = document.getElementById('nav');
-// const navLinks = document.getElementById('nav-links');
-
-// nav.addEventListener('click', () => {
-//     if (window.innerWidth <= 600) {
-//         if (navLinks.classList.contains('not-active-link')) {
-//             navLinks.classList.add('active-link');
-//             navLinks.classList.remove('not-active-link');
-//             navLinks.style.cssText += "transform: translateY(20px);display:inline-block;"
-//         } else {
-//             navLinks.classList.add('not-active-link');
-//             navLinks.classList.remove('active-link');
-//             navLinks.style.cssText += "transform: translateY(-20px);display:inline-block;"
-//         }
-//     }
-// });
