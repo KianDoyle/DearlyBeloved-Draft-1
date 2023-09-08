05dea72f93a252288b8ae643fde06dafb950c46e
KianDoyle <kianrk@icloud.com>
2023-09-08 11:38:38 +0100

    Logo image and polaroid update

--- index.js
+++ index.js
@@ -3,57 +3,74 @@ const background = document.querySelector("#background");
 const logoImg = document.querySelector("#logo-img");
 const polaroid = document.querySelector("#polaroid-wrapper");
 
-const anim1 = gsap.timeline();
-anim1.to(logoText, {
-    opacity: 0,
-    y: "100%", 
-    display: "none", 
-    duration: 0.5, 
-    ease: "ease.out", 
-    delay: 1, 
-});
 
-const anim2 = gsap.timeline();
-anim2.to(background, {
-    width: "825px", 
-    duration: 1, 
-    ease: "power2.inOut", 
-    delay: 1.5, 
-    onComplete: function () {
-        background.style.overflow = "visible";
-    }
-});
 
-const anim3 = gsap.timeline();
-anim3.to(logoImg, {
-    width: "825px", 
-    filter: "blur(15px)", 
-    duration: 1, 
-    ease: "power2.inOut", 
-    delay: 1.5, 
-});
 
-const anim4 = gsap.timeline();
-anim4.to(polaroid, {
-    top: "90px",
-    duration: 0.9, 
-    ease: "ease.inOut",
-    delay: 2.2,
-}).to(polaroid, {
-    top: "100px",
-    duration: 0.2,
-    ease: "bounce.out",
-});
+
+// const anim3mob = gsap.timeline();
+// anim3mob.to(logoImg, {
+//     filter: "blur(15px)",
+//     duration: 1, 
+//     delay: 1.5,
+//     ease: "power2.inout",
+// });
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
     const polaroidWrapper = document.getElementById('polaroid-wrapper');
     const removeButton = document.getElementById('remove-welcome-blurb');
     const imageWrapper = document.getElementById('portfolio-images');
 
-    anim1.play();
-    anim2.play();
-    anim3.play();    
-    anim4.play();
+    // Move logo text
+    const anim1 = gsap.timeline();
+    anim1.to(logoText, {
+        opacity: 0,
+        y: "100%",
+        display: "none",
+        duration: 0.5,
+        ease: "ease.out",
+        delay: 1,
+    });
+
+    // Check for window size
+    if (window.innerWidth > 800) {
+        // Background animation
+        const anim2 = gsap.timeline();
+        anim2.to(background, {
+            width: "825px", 
+            duration: 1, 
+            ease: "power2.inOut", 
+            delay: 1.5, 
+            onComplete: function () {
+                background.style.overflow = "visible";
+            }
+        });
+
+        // Logo image animation
+        const anim3 = gsap.timeline();
+        anim3.to(logoImg, {
+            width: "825px", 
+            filter: "blur(15px)", 
+            duration: 1, 
+            ease: "power2.inOut", 
+            delay: 1.5, 
+        });
+
+        console.log(window.innerWidth);
+    } 
+    // Polaroid animation
+    const anim4 = gsap.timeline();
+    anim4.to(polaroid, {
+        top: "90px",
+        duration: 0.9,
+        ease: "ease.inOut",
+        delay: 2.2,
+    }).to(polaroid, {
+        top: "100px",
+        duration: 0.2,
+        ease: "bounce.out",
+    });
 
     removeButton.addEventListener('click', () => {
         requestAnimationFrame(() => {
